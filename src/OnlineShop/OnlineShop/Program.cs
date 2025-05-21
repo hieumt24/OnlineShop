69b651b9ea99505ebe9f121152cd7b4df9abef37
hieu.maitrong <hieu.maitrong@nashtechglobal.com>
2025-05-21 22:46:54 +0700

    Edit Product

--- src/OnlineShop/OnlineShop/Program.cs
+++ src/OnlineShop/OnlineShop/Program.cs
@@ -21,16 +21,18 @@ app.UseRouting();
 
 app.UseAuthorization();
 
+
 app.UseEndpoints(endpoints =>
 {
     endpoints.MapControllerRoute(
         name : "Admin",
-        pattern : "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+        pattern : "{area:exists}/{controller=Homes}/{action=Index}/{id?}"
     );
 });
-
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+
 
 app.Run();
